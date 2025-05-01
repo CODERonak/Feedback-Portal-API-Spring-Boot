e9d37ad3383f775e0590edb9da87117a7a38ca2f
r9988.2588 <r9988.2588@gmail.com>
2025-05-01 08:23:20 +0000

    first commit

---
+++ src/main/java/com/code/FeedbackPortalAPI/FeedbackPortalApiApplication.java
@@ -0,0 +1,12 @@
+package com.code.FeedbackPortalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackPortalApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/code/FeedbackPortalAPI/FeedbackPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.FeedbackPortalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
